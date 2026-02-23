335d5ec1d4dac52d8d5988844bf9e39610633206
Paulo Araya <61099291+MotelPaso@users.noreply.github.com>
2026-02-23 17:03:47 -0300

    feat: change database to add guild_id change response of slash commands to actually show a response in the chat added a stats commands to show the time spent on the server voice channels

--- main.py
+++ main.py
@@ -15,28 +15,31 @@ active_sessions = {}
 
 class Client(commands.Bot):
     async def on_ready(self):
-        print(f"Connecting to database:")
+        print(f"Connecting to database: ...")
         self.db = await asyncpg.connect(DATABASE_URL)
-
         await self.db.execute(
             """
             CREATE TABLE IF NOT EXISTS voice_stats (
-                id BIGINT PRIMARY KEY,
+                id BIGINT,
+                guild_id BIGINT,
                 name TEXT NOT NULL,
-                seconds_in_call REAL DEFAULT 0)
+                seconds_in_call REAL DEFAULT 0,
+                PRIMARY KEY (id, guild_id))
             """)
-        print(f'We have logged in as {client.user}')
+        print(f'Connected! We have logged in as {client.user}')
         try:
             synced = await self.tree.sync()
             print(f'synced {len(synced)} global commands')
         except Exception as e:
             print(f'Error in sync: {e}')
+
     async def on_message(self, message):
         if message.author == client.user:
             return
         if message.content == "ping":
             latency = client.latency * 1000
             await message.channel.send(f"ping: {round(latency,2)}ms")
+
     async def on_voice_state_update(self, member, before, after):
         current_time:float = time.time()
         if before.channel == None and after.channel != None: # was not in a channel and joined
@@ -48,9 +51,9 @@ class Client(commands.Bot):
             duration = current_time - time_joined
             await self.db.execute(
                 """
-                INSERT INTO voice_stats (id, name, seconds_in_call) VALUES ($1, $2, $3)
-                ON CONFLICT (id) DO UPDATE SET seconds_in_call = voice_stats.seconds_in_call + $3, name = $2
-                """, member.id, member.name, duration)
+                INSERT INTO voice_stats (id, guild_id, name, seconds_in_call) VALUES ($1, $2, $3, $4)
+                ON CONFLICT (id, guild_id) DO UPDATE SET seconds_in_call = voice_stats.seconds_in_call + $4, name = $3
+                """, member.id, member.guild.id, member.name, duration)
 
 
 intents = discord.Intents.default()
@@ -64,10 +67,9 @@ async def join(interaction: discord.Interaction):
     user = interaction.user
     channel = interaction.channel
     if (user.voice == None):
-        await channel.send("Que no estas en un canal tonto polla")
+        await interaction.response.send_message("Que no estas en un canal tonto polla")
     elif (client.voice_clients != []):
-        await channel.send("Ya estoy en un canal!")
-
+        await interaction.response.send_message("Ya estoy en un canal!")
     else:
         await interaction.response.send_message("Uniendome al canal...")
         voice_channel = user.voice.channel
@@ -79,17 +81,17 @@ async def disconnect(interaction: discord.Interaction):
     channel = interaction.channel
     channel_list = client.voice_clients
     if (channel_list == []):
-        await channel.send("El bot no esta conectado a ningun canal.")
+        await interaction.response.send_message("El bot no esta conectado a ningun canal.")
     elif (user.voice == None): # usuario no conectado
         channel = interaction.channel
-        await channel.send("Que no estas en un canal tonto polla")
+        await interaction.response.send_message("Que no estas en un canal tonto polla")
     else:
         await interaction.response.send_message("Saliendo del canal...")
         current_channel = channel_list[0]
         await current_channel.disconnect()
         current_channel.cleanup()
 
-@client.tree.command(name="call", description="Llama a un miembro del servidor")
+@client.tree.command(name="call", description="Llama a un miembro del servidor!")
 async def call(interaction: discord.Interaction, member: discord.Member):
     await interaction.response.send_message(f'llamando a {member.name}')
     try:
@@ -97,5 +99,16 @@ async def call(interaction: discord.Interaction, member: discord.Member):
     except Exception as e:
         print("error...")
         print(e)
+
+@client.tree.command(name="stats", description="Revisa el tiempo que has pasado en una llamada!" )
+async def stats(interaction: discord.Interaction):
+    user = interaction.user
+    user_stats = await client.db.fetch(
+        "SELECT name, seconds_in_call FROM voice_stats WHERE (id = ($1) AND guild_id = ($2))", user.id, user.guild.id)
+    response = f"Nombre: {user.name}\nTiempo pasado en llamada:"
+    if not user_stats:
+        await interaction.response.send_message(f"{response} 0 segundos!")
+    else:
+        await interaction.response.send_message(f"{response} {int(user_stats[0]["seconds_in_call"])} segundos")
 
 client.run(token)
